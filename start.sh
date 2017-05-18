553251409909e3f3bd99514f44999caab62bcd56
nbonfils <nils.bonfils@epfl.ch>
2017-05-18 19:32:57 +0200

    update start and install script to use new mysql db

--- start.sh
+++ start.sh
@@ -4,5 +4,13 @@
 
 #python django_example/manage.py runserver
 
+ssh -fNAL 3306:127.0.0.1:3306 ubuntu@sdf-bill2myprint-mysql-1
+
 source ./.venv/bin/activate
 python3 src/manage.py runserver 0.0.0.0:8000
+
+PID=`pgrep -f "ssh -fNAL 3306:127.0.0.1:3306 ubuntu@sdf-bill2myprint-mysql-1"`
+if [[ "" !=  "$PID" ]]; then
+  echo "killing $PID"
+  kill $PID
+fi
