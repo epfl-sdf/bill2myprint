38ee54cd5ed66a60a297b1b89e73b8e719c1d449
yaanst <yaanst@gmail.com>
2017-05-19 02:58:12 +0200

    Finish detailed view for faculties, including the computation of faculty cost

--- src/bill2myprint/views.py
+++ src/bill2myprint/views.py
@@ -15,7 +15,7 @@ from django.core.cache import cache
 
 
 def index(request):
-    context = {'text': 'Hello world !'}
+    context = {}
     return render(request, 'bill2myprint/index.html', context)
 
 def students(request):
@@ -53,6 +53,70 @@ def faculties(request):
                                  )
             context['faculties'] = data
     return render(request, 'bill2myprint/faculties.html', context)
+
+def detail(request):
+    context = {}
+    context['semesters'] = Semester.objects.values_list('name', flat=True)
+    context['faculties'] = Faculty.objects.values_list('name', flat=True)
+    if request.POST:
+        semester_asked = request.POST.getlist('semesters[]')
+        semester_objects = [Semester.objects.get(name=s) for s in semester_asked]
+        faculty_objects = Faculty.objects.get(name=request.POST.get('faculties[]'))
+        section_objects = faculty_objects.section_set.all()
+        if semester_asked and faculty_objects:
+            data = Semester.objects.none()
+            data = SemesterSummary.objects.filter(section__in=section_objects, semester__in=semester_objects)
+            data = data.values('student__sciper', 
+                    'section__acronym', 
+                    'semester__name',
+                    'myprint_allowance',
+                    'faculty_allowance',
+                    'total_spent')
+
+            def compute_fac_cost(student):
+                vpsi = student['myprint_allowance']
+                fac = student['faculty_allowance']
+                spent = round(student['total_spent'], 2)
+                student['total_spent'] = spent # update to rounded value
+                
+                fac_cost = 0.0
+                diff_vpsi = spent + vpsi
+                if diff_vpsi < 0:       # used all of vpsi credit
+                    if abs(diff_vpsi) < fac: # used part of fac credit only
+                        fac_cost = -round(diff_vpsi, 2)
+                    else:
+                        fac_cost = round(fac, 2)  # used all of fac credit
+                student['faculty_cost'] = fac_cost
+                return student
+
+            student_data = []
+            section_data = {}
+            
+            for student in data:
+                # Add the faculty_cost field to each student
+                student_data.append(compute_fac_cost(student))
+
+                # Aggregate the faculty_cost by section
+                section = student['section__acronym']
+                if section in section_data:
+                    section_data[section]['faculty_cost'] += student['faculty_cost']
+                else:
+                    section_data[section] = {
+                            'section__acronym': section,
+                            'faculty_cost': student['faculty_cost']
+                            }
+
+            for section in section_data.values():
+                section['faculty_cost'] = round(section['faculty_cost'], 2)
+
+            context['sections'] = section_data.values()
+
+            if request.POST.get('student', False):
+                context['students'] = student_data
+
+
+    return render(request, 'bill2myprint/detail.html', context)
+        
 
 #class SectionsView(LoginRequiredMixin, ListView):
 #    # Template attributes
