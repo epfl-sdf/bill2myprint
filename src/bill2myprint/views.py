d7eec29a900b4da4a3c345fd6f460ade31357b96
yaanst <yaanst@gmail.com>
2017-05-19 03:26:20 +0200

    Clean and comment views Add button to /detail in index.html Rephrased sentence in detail.html

--- src/bill2myprint/views.py
+++ src/bill2myprint/views.py
@@ -1,5 +1,5 @@
 import time
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render
 # Authentication imports
 from django.contrib.auth.decorators import login_required # for custom views (functions)
 from django.contrib.auth.mixins import LoginRequiredMixin # for generic views (classes)
@@ -10,7 +10,7 @@ from django.views.generic import ListView
 from uniflow.models import ServiceconsumerT, BudgettransactionsT
 from bill2myprint.models import *
 
-from django.db.models import Sum, Case, When, F, Q, FloatField, Value
+from django.db.models import Sum
 from django.core.cache import cache
 
 
@@ -21,10 +21,14 @@ def index(request):
 def students(request):
     context = {}
     context['semesters'] =  Semester.objects.values_list('name', flat=True)
+
     if request.POST:
+        # Get all the semesters selected as objects
         semesters_asked = request.POST.getlist('semesters[]')
         semesters_objects = [Semester.objects.get(name=s) for s in semesters_asked]
+
         if semesters_asked:
+            # Get all SemesterSummary entries with the corresponding semesters
             data = Semester.objects.none()
             data = SemesterSummary.objects.filter(semester__in=semesters_objects)
             data = data.values('student__sciper',
@@ -40,12 +44,17 @@ def faculties(request):
     context = {}
     context['semesters'] =  Semester.objects.values_list('name', flat=True)
     if request.POST:
+        # Get all the semesters selected as objects
         semester_asked = request.POST.getlist('semesters[]')
         semesters_objects = [Semester.objects.get(name=s) for s in semester_asked]
+
         if semester_asked:
+            # Get all SemesterSummary entries with the corresponding semesters
             data = Semester.objects.none()
             data = SemesterSummary.objects.filter(semester__in=semesters_objects)
             data = data.values('section__faculty__name', 'semester__name')
+
+            # Aggregate by summing
             data = data.annotate(vpsi=Sum('myprint_allowance'),
                                  fac=Sum('faculty_allowance'),
                                  perso=Sum('total_charged'),
@@ -59,11 +68,18 @@ def detail(request):
     context['semesters'] = Semester.objects.values_list('name', flat=True)
     context['faculties'] = Faculty.objects.values_list('name', flat=True)
     if request.POST:
+        # Get all the semesters selected as objects
         semester_asked = request.POST.getlist('semesters[]')
         semester_objects = [Semester.objects.get(name=s) for s in semester_asked]
+
+        # Get the faculty selected as object
         faculty_objects = Faculty.objects.get(name=request.POST.get('faculties[]'))
+
+        # Get all sections from that faculty
         section_objects = faculty_objects.section_set.all()
+
         if semester_asked and faculty_objects:
+            # Get all SemesterSummary entries with the corresponding section and semester
             data = Semester.objects.none()
             data = SemesterSummary.objects.filter(section__in=section_objects, semester__in=semester_objects)
             data = data.values('student__sciper', 
@@ -74,6 +90,11 @@ def detail(request):
                     'total_spent')
 
             def compute_fac_cost(student):
+                """
+                Function computing the amount the faculty will be billed
+                for the given student.
+                It adds the key,value pair: 'faculty_cost' -> amount
+                """
                 vpsi = student['myprint_allowance']
                 fac = student['faculty_allowance']
                 spent = round(student['total_spent'], 2)
@@ -106,37 +127,18 @@ def detail(request):
                             'faculty_cost': student['faculty_cost']
                             }
 
+            # Round all values of faculty costs
             for section in section_data.values():
                 section['faculty_cost'] = round(section['faculty_cost'], 2)
 
             context['sections'] = section_data.values()
 
+            # Send detail by student only if asked
             if request.POST.get('student', False):
                 context['students'] = student_data
 
-
     return render(request, 'bill2myprint/detail.html', context)
         
-
-#class SectionsView(LoginRequiredMixin, ListView):
-#    # Template attributes
-#    template_name = 'bill2myprint/sections.html'
-#    context_object_name = 'section_list'
-#
-#    def get_queryset(self):
-#        # The "S_StudU" part is prevented from being displayed with to the "cut" templatetag
-#        return ServiceconsumerT.objects.filter(name__endswith='S_StudU')
-
-
-#class RallongeFacultaireView(LoginRequiredMixin, ListView):
-#    template_name = 'bill2myprint/rallonge_facultaire.html'
-#    context_object_name = 'rallonge_list'
-#    paginate_by = 20
-#
-#    def get_queryset(self):
-#        # Get the rallonge facultaire in the BudgetTransaction table
-#        return BudgettransactionsT.objects.filter(transactiondata__startswith='Rallonge')
-
 
 class RallongeFacultaire2View(LoginRequiredMixin, ListView):
     template_name = 'bill2myprint/rallonge_facultaire.html'
@@ -145,36 +147,3 @@ class RallongeFacultaire2View(LoginRequiredMixin, ListView):
     def get_queryset(self):
         # Get the rallonge facultaire in the BudgetTransaction table
         return BudgettransactionsT.objects.filter(transactiondata__startswith='Rallonge').values('amount', 'transactiondata').distinct()
-
-
-#class StudentsView(LoginRequiredMixin, ListView):
-#    template_name = 'bill2myprint/etudiants.html'
-#    context_object_name = 'students_list'
-#    paginate_by = 15
-#
-#    def get_queryset(self):
-#        # Get the students list
-#        students_group_id = ServiceconsumerT.objects.get(name='Etudiant').id
-#        students_cost_center_id = ServiceconsumerT.objects.get(name='ETU').id
-#        return ServiceconsumerT.objects.filter(defaultgroupid=students_group_id, defaultcostcenter=students_cost_center_id)
-
-
-#class StudentDetailView(LoginRequiredMixin, ListView):
-#    template_name = 'bill2myprint/etudiant_detail.html'
-#    context_object_name = 'transactions_list'
-#    paginate_by = 15
-#
-#    def get_queryset(self):
-#        # Get the student's transaction history
-#        student_id = self.kwargs['studid']
-#        return ServiceconsumerT.objects.get(pk=student_id).budgettransactionst_set.all()
-#
-#
-#@login_required
-#def studentTotal(request, studid):
-#    # Output total of student depense
-#    template_name = 'bill2myprint/etudiant_total.html'
-#    student = ServiceconsumerT.objects.get(pk=studid)
-#    total = student.budgettransactionst_set.aggregate(Sum('amount'))['amount__sum']
-#    name = student.name
-#    return render(request, template_name, {'total': total, 'name': name})
